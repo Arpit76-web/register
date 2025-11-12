da2b10266dcac097725bb454c4354660ecae963b
Arpit76-web <singh7898arpit@gmail.com>
2025-11-12 22:47:32 +0530

    line fix

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected and you do work on your backend now');
+    console.log('MongoDB connected');
   } catch (err) {
     console.error('MongoDB connection error:', err);
     process.exit(1);
